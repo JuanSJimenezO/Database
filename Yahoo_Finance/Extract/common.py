08334f1257687535695da4a64d37c2ba7a2bdd25
JuanSJimenezO <131063735+JuanSJimenezO@users.noreply.github.com>
2023-04-17 18:48:28 -0400

    Add files via upload

---
+++ Yahoo_Finance/Extract/common.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 17 17:16:45 2023
+
+@author: Saint90
+"""
+
+import json
+import logging
+
+logging.basicConfig(
+    filename = "Log.log",
+    filemode = "w",
+    format = "%(asctime)s | %(module)s | %(levelname)s | %(message)s",
+    level = logging.INFO
+    )
+
+logger = logging.getLogger(__name__)
+
+__config = None
+
+def config():
+    
+    global __config
+    
+    if not __config: 
+        
+        with open("config.json", mode = "r") as file:
+            __config = json.loads(file.read())
+            file.close()
+    
+    return __config
